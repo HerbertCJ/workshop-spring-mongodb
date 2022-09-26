9d9969b0900171432d9f30dc67be749fa3fefbaf
Herbert Cabus Jacby <herbertcj3@gmail.com>
2022-09-26 18:24:24 -0300

    Obtendo Post por Id

--- src/main/java/com/herbert/workoshopmongo/repository/UserRepository.java
+++ src/main/java/com/herbert/workoshopmongo/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.herbert.workoshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.herbert.workoshopmongo.domain.Post;
 import com.herbert.workoshopmongo.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String>{	
 	
 }
